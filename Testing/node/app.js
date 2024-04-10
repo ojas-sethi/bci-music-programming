b72a3f5d69e66559bcb7fd1f598245bcc0bc77f3
Ntropy86 <nitigya.199301207@muj.manipal.edu>
2024-04-09 19:40:10 -0500

    📦`TGC Check✅`

--- Testing/node/app.js
+++
@@ -1,8 +0,0 @@
-const { createClient } = require('node-thinkgear-sockets');
-const { fromEvent } = require('rxjs/observable/fromEvent');
-
-const client = createClient();
-client.connect();
-
-fromEvent(client, 'data')
-    .subscribe(eeg => console.log(eeg));
